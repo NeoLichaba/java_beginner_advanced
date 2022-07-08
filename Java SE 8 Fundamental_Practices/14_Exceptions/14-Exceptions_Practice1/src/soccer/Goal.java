8dae1c7caa5d336c15c50f5b57bec050185137c2
Neo Lichaba <neolicham@gmail.com>
2022-07-09 00:41:40 +0200

    completed soccer app uploaded

---
+++ Java SE 8 Fundamental_Practices/14_Exceptions/14-Exceptions_Practice1/src/soccer/Goal.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package soccer;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Goal extends GameEvent {
+    
+        public String toString(){
+        return "Goal scored";
+    }
+}
---
+++ Java SE 8 Fundamental_Practices/14_Exceptions/14-Exceptions_Practice1/src/utility/PlayerDatabaseException.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utility;
+
+/**
+ *
+ * @author User
+ */
+public class PlayerDatabaseException extends Exception {
+
+    public PlayerDatabaseException() {
+    }
+
+    public PlayerDatabaseException(String message) {
+        super(message);
+    }
+
+}
