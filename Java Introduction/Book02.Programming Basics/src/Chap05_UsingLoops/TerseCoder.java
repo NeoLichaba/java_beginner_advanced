32f0b6daf3907d51dd6d6969ac090b81ec57000b
Neo Lichaba <neolicham@gmail.com>
2022-07-19 16:28:13 +0200

    TerseCoder file uploaded

---
+++ Java Introduction/Book02.Programming Basics/src/Chap05_UsingLoops/TerseCoder.java
@@ -0,0 +1,11 @@
+package Chap05_UsingLoops;
+
+/* Program demonstrates use of for loops without a statement  
+ */
+
+public class TerseCoder {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; System.out.println(i++));              //count expression is the sout method that uses an increment operator
+    }
+}
