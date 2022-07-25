67ecbf1cb943883aae651964f1cf70b75122a057
Neo Lichaba <neolicham@gmail.com>
2022-07-26 00:43:58 +0200

    Singleton04-03Prac exercise uploaded

--- Java Advanced Course Work/Practices/Singleton04-03Prac/src/com/example/Main.java
+++ Java Advanced Course Work/Practices/Singleton04-03Prac/src/com/example/Main.java
@@ -1,7 +1,24 @@
 package com.example;
 
 public class Main {
-  public static void main(String[] args) {
-    
-  }  
+
+    public static void main(String[] args) {
+
+        PeerSingleton peerList01 = PeerSingleton.getInstance();
+
+        PeerSingleton peerList02 = PeerSingleton.getInstance();
+
+        //host names displayed using a for loop
+        for (String hostName : peerList01.getHostNames()) {
+            System.out.println("Host name: " + hostName);
+        }
+        
+        //host names displayed using a for loop
+        System.out.println();
+        for (String hostName : peerList02.getHostNames()) {
+            System.out.println("Host name: " + hostName);
+        }
+    }
+
 }
+
