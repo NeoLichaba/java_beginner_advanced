e5a555189d5f8c9de8c4d6bca1dd846728b00545
Neo Lichaba <neolicham@gmail.com>
2022-08-02 12:15:26 +0200

    ex04_01 completed and uploaded

---
+++ Java SE 8 Fundamental_Exercises/JavaIntro_ClassExercises/src/ex04_1_exercise/ShoppingCart.java
@@ -0,0 +1,20 @@
+package ex04_1_exercise;
+
+public class ShoppingCart {
+
+    public static void main(String[] args) {
+        // Declare and initialize String variables.  Do not initialize message yet.
+
+        String custName = "Ems Boogey";
+        String itemDesc = "Candy";
+        String message;
+
+        // Assign the message variable 
+        message = custName + " wants to purchase some " + itemDesc;
+
+        // Print and run the code
+        System.out.println(message);
+
+    }
+
+}
