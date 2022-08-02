71e09a817059f03a5dea881ae25d962bafb502dd
Neo Lichaba <neolicham@gmail.com>
2022-08-02 12:48:15 +0200

    ex06_2 completed and uploaded

---
+++ Java SE 8 Fundamental_Exercises/JavaIntro_ClassExercises/src/ex06_1_exercise/items.java
@@ -0,0 +1,13 @@
+package ex06_1_exercise;
+
+public class items {
+    public int ID;
+    public String descr;
+    public int quantity;
+    public double price;
+    
+    public void displayItems (){
+        System.out.println(ID + ":" + descr);
+    }
+            
+}
