cc8e27a5ae6344176411de70948df5550d6777f6
Neo Lichaba <neolicham@gmail.com>
2022-09-28 20:01:06 +0200

    Using Month enumeration

--- Java Introduction/Book5.ProgrammingTechniques/src/Chap04_DatesAndTimes/Notes.java
+++ Java Introduction/Book5.ProgrammingTechniques/src/Chap04_DatesAndTimes/Notes.java
@@ -1,9 +1,28 @@
 
 package Chap04_DatesAndTimes;
 
-/**
- *
- * @author User
+/* Using the parse Method to Create a Date-Time Object
+   -----------------------------------------------------
+   * Creates a Date-Time object from a string that represents a specific date or time
+   * e.g. LocalDateTime dt;
+          dt = LocalDateTime.parse("2014-12-15T15:45"); - T separates the date from the time, time expressed in 24 hour format
+   * If String not in correct format, parse method throws a DateTimeParseException. parse method must be enclosed in a try/catch block
+   * parse Method useful for converting user input to a Date-Time object
+
+*  Using the of Method to Create a Date-Time Object
+   -----------------------------------------------------
+    e.g. LocalDate date = LocalDate.of(2014,12,15);
+
+    Using the Month enumeration
+    ------------------------------
+    * Month is an enumeration that represents the twelve months of the year
+      LocalDate date = LocalDate.of(2014,Month.DECEMBER,15);
+
+    * System.out.println("December hath "
+        + Month.DECEMBER.length(false) + " days."); 
+        - boolean argument in length method indicates whether calculation should be for a leap year
+
+* @author Neo
  */
 public class Notes {
     
