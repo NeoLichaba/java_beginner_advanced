dcd63ffe76dae0d2dd63973ff0f84c7c58c28233
Neo Lichaba <neolicham@gmail.com>
2022-09-28 21:25:17 +0200

    Extracting Info About a Date

--- Java Introduction/Book5.ProgrammingTechniques/src/Chap04_DatesAndTimes/Notes.java
+++ Java Introduction/Book5.ProgrammingTechniques/src/Chap04_DatesAndTimes/Notes.java
@@ -21,7 +21,51 @@ package Chap04_DatesAndTimes;
     * System.out.println("December hath "
         + Month.DECEMBER.length(false) + " days."); 
         - boolean argument in length method indicates whether calculation should be for a leap year
+    
+    Using the ZoneId class (refer to P547 for example code)
+    -------------------------------------------------------
+    * To create ZonedDateTime, create a time zone object by using the ZoneId class
+    * Know standard name of the time zone you want to create
 
+    
+    Using the ZoneOffSet class 
+    ------------------------------------------------------------
+    *The of method OffsetTime and OffsetDateTime classes use an additional class 
+     named ZoneOffset to indicate the offset from UTC. You can create a ZoneOffset
+    by using any of the following methods of the ZoneOffset class:
+        » of(String offsetId)
+        » ofHours(int hours)
+        » ofHoursMinutes(int hours, int minutes)
+        » ofHoursMinutesSeconds(int hours, int minutes, int seconds)
+        » ofTotalSeconds(int totalSeconds)
+
+    * For example, you can create a ZoneOffset of -8 hours like this:
+      ZoneOffset z = ZoneOffset.ofHours(-8)
+    * Note that when you use a string offset, you must provide two digits for the hours, 
+      minutes, and (optionally) seconds
+    * Once you have a ZoneOffset object, you can use it to create an OffsetTime, as in 
+      this example:
+      OffsetTime time = OffsetTime.of(10, 30, 0, 0, z);
+    
+    Looking Closer at the LocalDate Class (P548)
+    ------------------------------------------------------------
+    
+    Extracting Information About a Date (P550)
+    ----------------------------------------------------
+    LocalDate date = LocalDate.now();
+    int year = date.getYear();
+    int month = date.getMonthValue();
+    int day = date.getDayOfMonth();
+
+    If you need to know how many days into the year a particular date is, you can use 
+    this code:
+    LocalDate date = LocalDate.parse("2016-04-09");
+    System.out.println(date.getDayOfYear());
+    
+    getDayOfWeek method returns a value of type DayOfWeek
+    System.out.println(date.getDayOfWeek());
+
+    
 * @author Neo
  */
 public class Notes {
