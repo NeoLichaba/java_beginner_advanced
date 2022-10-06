ed9599f4b27216f02ebdd1d8ac5f284c4fc877de
Neo Lichaba <neolicham@gmail.com>
2022-10-06 16:52:56 +0200

    CalculatingDates revised with notes

--- Java Introduction/Book5.ProgrammingTechniques/src/Chap04_DatesAndTimes/P553_CalculatingDates.java
+++ Java Introduction/Book5.ProgrammingTechniques/src/Chap04_DatesAndTimes/P553_CalculatingDates.java
@@ -1,28 +1,30 @@
 package Chap04_DatesAndTimes;
 
 import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
+import java.time.temporal.ChronoUnit;                                           
 
-/*
+/* Program displays a business that prepares invoices on the 15th of each month
  *
- * @author User
+ * @author Neo
  */
 public class P553_CalculatingDates {
 
     public static void main(String[] args) {
-        LocalDate today = LocalDate.now();                                      //calling the now method
-        LocalDate invDate = LocalDate.of(today.getYear(),
-                today.getMonthValue(), 15);
-        if (today.getDayOfMonth() > 15) {
-            invDate = invDate.plusMonths(1);
+        LocalDate today = LocalDate.now();                                      //current date obtained, calling now method of the LocalDate class
+        LocalDate invDate = LocalDate.of(today.getYear(),                       //invoice created displaying/retrieving Year and Month value, setting current date to the 15th
+                today.getMonthValue(), 15);                                     
+        if (today.getDayOfMonth() > 15) {                                       //if today's date of the year is after the 15th, add 1 month to invoicing date
+                                                                                //i.e. invoice date is the next month
+            invDate = invDate.plusMonths(1);                                    //+1 month added to invoice date
         }
-        long daysToInvoice = today.until(invDate,
-                ChronoUnit.DAYS);
+        long daysToInvoice = today.until(invDate,                               //until method used to determine number of dates between current date and next invoice date
+                ChronoUnit.DAYS);                                               //ChronoUnit- enum defines units of time 
         System.out.println(daysToInvoice
                 + " until next invoice date.");
 
-        LocalDate date1 = LocalDate.parse("2014-05-16");
-        LocalDate date2 = LocalDate.parse("2014-12-15");
-        System.out.println(date1.until(date2, ChronoUnit.DAYS));
+        LocalDate date1 = LocalDate.parse("2022-10-01");                        
+        LocalDate date2 = LocalDate.parse("2022-12-31");
+        System.out.println(date1.until(date2, ChronoUnit.DAYS));                //number of days between date1 and date2 printed
+                                                                                
     }
 }
--- Java Introduction/Book5.ProgrammingTechniques/src/Chap04_DatesAndTimes/P555_FormatDateTime.java
+++ Java Introduction/Book5.ProgrammingTechniques/src/Chap04_DatesAndTimes/P555_FormatDateTime.java
@@ -3,7 +3,7 @@ package Chap04_DatesAndTimes;
 import java.time.LocalDateTime;                                                 //implements ChronoLocalDateTime interface
 import java.time.format.DateTimeFormatter;
 
-/**Fix**/
+/*
 /* Program will print out current date in several formats
  *
  * @author Neo
@@ -23,7 +23,7 @@ public class P555_FormatDateTime {
     public static void printDate(LocalDateTime date, String pattern) {
         DateTimeFormatter f;                                                    //variable f stored in custom format created in DateFormatter class 
         f = DateTimeFormatter.ofPattern(pattern);                               //f is created using the static ofPattern method, passing pattern in it's parameter
-        pattern = (pattern + " ").substring(0, 11);                             //spaces added to pattern string and 11 character-long substring is taken starting at the first character (for alignment)
+        pattern = (pattern + "              ").substring(0, 14);                //spaces added to pattern string and 11 character-long substring is taken starting at the first character (for alignment)
         System.out.println(pattern + " " + date.format(f));                     //date formated using specified formatter
     }
 }
