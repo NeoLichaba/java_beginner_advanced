4c8947d89228d28bf7e0bfc815ce3bb8647b9ad9
Neo Lichaba <neolicham@gmail.com>
2022-10-06 22:28:14 +0200

    Notes up to singleton selects - mySQL uploaded

--- Java Introduction/Book08.FilesAndDatabases/src/Chap03_DatabaseFor100USD/Notes.java
+++ Java Introduction/Book08.FilesAndDatabases/src/Chap03_DatabaseFor100USD/Notes.java
@@ -11,6 +11,35 @@ package Chap03_DatabaseFor100USD;
  * ; Required only when you use SQL statements in a script or interactively from the 
     MySQL Command Line Client program
 
+    Querying a DB
+    -----------------------------
+  - A query is an operation performed against one or more SQL tables; it extracts 
+    data from the tables and creates a result set
+  - SELECT statement - 
+    e.g select title, year - columns you want to include in the query result
+        from movie - table from which you want to retrive the rows
+        order by year; - sorted by year column
+    
+    Narrowing the query
+    -----------------------------
+  - To select certain rows from a table, use the where clause
+
+    Excluding rows
+    -----------------------------
+  - retrieve rows except those that match a certain criteria 
+    e.g select title, year from movie
+           ->where year < 1970 or year > 1979
+            -> order by year;
+ 
+    Using singleton selects
+    ------------------------------------
+  -  When you want to retrieve information for a specific row, mention the primary 
+     key column in the where clause, like this:
+     mysql> select title, year from movie where id = 7
+     where clause selects the row whose id column equals 7
+     select statement is called a singleton select because it retrieves only one row.
+     allow users to access or update a specific database row
+     
  * @author User
  */
 public class Notes {
--- Java Introduction/Book08.FilesAndDatabases/src/Chap03_DatabaseFor100USD/P830_DBCreationScript.java
+++ Java Introduction/Book08.FilesAndDatabases/src/Chap03_DatabaseFor100USD/P830_DBCreationScript.java
@@ -2,20 +2,22 @@ package Chap03_DatabaseFor100USD;
 
 /**
  *
- * @author User
+ * @author Neo
  */
 public class P830_DBCreationScript {
-    /*    drop database if exists movies;                                       //drop statment - deletes any exisiting DB with the same name
+       drop database if exists movies;                                       //drop statment - deletes any exisiting DB with the same name
 create database movies;                                                         //new DB created named movies
 use movies;                                                                     //use = script statements that follow apply to movie DB
-create table movie ( //→4
- id int not null auto_increment, //→5
- title varchar(50), //→6
- year int, //→7
- price decimal(8,2), //→8
- primary key(id) //→9
+create table movie (                                                            //create table statement creates table names movie with columns; primary key is id column
+ id int not null auto_increment,                                                //id columm data type = int; not null = value to exist for every row; 
+                                                                                //auto-increment - DB server provides values
+                                                                                //Everytime new row added to the table, value for id column incremented                             
+ title varchar(50),                                                             //varchar data type for title column
+ year int,                                                                      //year's column data type is int
+ price decimal(8,2),                                                            //decimal data type for price - converted to double
+ primary key(id)                                                                //id column = primary key - unique value
 );
-insert into movie (title, year, price) //→11
+insert into movie (title, year, price)                                          //insert - add data to the database
  values ("It's a Wonderful Life", 1946, 14.95);
 insert into movie (title, year, price)
  values ("Young Frankenstein", 1974, 16.95);
@@ -35,5 +37,5 @@ insert into movie (title, year, price)
  values("The King's Speech",2010,17.85);
 insert into movie (title, year, price)
  values("Star Trek Into Darkness",2013,19.95);
-}*/
+}
 }
