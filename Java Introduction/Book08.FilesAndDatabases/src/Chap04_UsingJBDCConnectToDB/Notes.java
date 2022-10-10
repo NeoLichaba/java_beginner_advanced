e9789d231f73112bbdfd1559a22a4436c65530bb
Neo Lichaba <neolicham@gmail.com>
2022-10-10 23:39:29 +0200

    Listing 4.1 ListMovies uploaded

--- Java Introduction/Book08.FilesAndDatabases/src/Chap04_UsingJBDCConnectToDB/Notes.java
+++ Java Introduction/Book08.FilesAndDatabases/src/Chap04_UsingJBDCConnectToDB/Notes.java
@@ -21,8 +21,65 @@ package Chap04_UsingJBDCConnectToDB;
          String url = "jdbc:odbc:Movies";
        * getConnection method throws SQLException
 
-       * 
- * @author User
+       *
+    
+  * Querying a Database
+    -----------------------------------
+    * After one has established a connection to the database, execute select statements 
+      to retrieve data. To do so, you have to use several classes and interfaces:
+        » Connection: The Connection class has two methods - close method closes the connection, 
+          and the createStatement method returns a Statement object, which you then use to execute statements.
+        » Statement: The Statement interface contains the methods necessary to send 
+        statements to the database for execution and return the results. In particular, 
+        you use the executeQuery method to execute a select statement or the 
+        executeUpdate method to execute an insert, update, or delete statement.
+        » ResultSet: The ResultSet interface represents rows returned from a query. 
+        It provides methods you can use to move from row to row and to get the data 
+        for a column
+
+    * Refer to Table 4-1 lists the methods of the Connection class and the Statement interface you use to execute qu
+    * The first parameter of the createStatement method specifies the type of result 
+        set that is created, and can be one of the following:
+        ResultSet.TYPE_FORWARD_ONLY
+        ResultSet.TYPE_SCROLL_INSENSITIVE
+        ResultSet.TYPE_SCROLL_SENSITIVE
+    * The second parameter indicates whether the result set is read-only or updatable, and can be one of the following:
+        ResultSet.CONCUR_READ_ONLY
+        ResultSet.CONCUR_UPDATABLE
+    ** Executing a select statement
+       --------------------------------- 
+        The following snippet executes a select statement and gets the result set:
+            Statement s = con.createStatement();
+            String select = "Select title, year, price "
+             + "from movie order by year";
+            ResultSet rows = s.executeQuery(select);
+            -- result set stored in rows variable
+       
+    ** Navigating through the result set
+       ------------------------------------------
+
+        - The ResultSet object returned by the executeQuery statement contains all the 
+        rows that are retrieved by the select statement.
+        - result set maintains a pointer called a cursor to keep track of the current row
+        - Use methods in Table 4-2 in order to move cursor through the results set
+        **  code that processes each row in a result set:
+            while(rows.next())
+            {
+             // process the current row
+             String title = row.getString("title");
+                int year = row.getInt("year");
+                double price = row.getDouble("price");
+
+            //assuming the columns appear in order:
+                String title = row.getString(1);
+                int year = row.getInt(2);
+                double price = row.getDouble(3);
+            }
+    ** Getting data from a result set
+        Table 4-3 lists the methods of the ResultSet interface you can use to retrieve 
+        data from the current row.
+
+* @author User
  */
 public class Notes {
     
--- Java Introduction/Book08.FilesAndDatabases/src/Chap04_UsingJBDCConnectToDB/P853_ListMovies.java
+++ Java Introduction/Book08.FilesAndDatabases/src/Chap04_UsingJBDCConnectToDB/P853_ListMovies.java
@@ -1,10 +1,94 @@
-
 package Chap04_UsingJBDCConnectToDB;
 
 /* Listing 4.1
- *
+ * Program reads data from the movies database and lists it on the console
+ * 
  * @author Neo
  */
+import java.sql.*;
+import java.text.NumberFormat;
+
 public class P853_ListMovies {
     
+    
+    public static void main(String[] args) 
+    {
+        NumberFormat cf = NumberFormat.getCurrencyInstance();
+        ResultSet movies = getMovies();                                         //getMovies method called to get a ResultSet object that contains the movies to be listed
+        try {
+            while (movies.next()) {                                             //each row read of the result set
+                Movie m = getMovie(movies);                                     //getMovie called to create a movie object from data in current row
+                String msg = Integer.toString(m.year);
+                msg += ": " + m.title;
+                msg += " (" + cf.format(m.price) + ")";
+                System.out.println(msg);                                        //output string created and sent to console
+            }
+        } catch (SQLException e) {                                              //next method throws a SQLException - try/catch statement
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static ResultSet getMovies() //→28                                  //getMovies methods obtains connection to DB
+                                                                                //Queries DB to get the movies
+    {
+        Connection con = getConnection();
+        try {
+            Statement s = con.createStatement();                                //statement created and executed with select statement
+            String select = "Select title, year, price "
+                    + "from movie order by year";
+            ResultSet rows;
+            rows = s.executeQuery(select);                                      //executeQuery executes select statement and returned as a ResultSet object
+            return rows;                                                        
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());                                 //statement created and executed
+        }
+        return null;
+    }
+
+    private static Connection getConnection()                                   //getConnection method created Connection object to DB
+    {                                                                           //credentials are hard coded - generally obtain from user
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://localhost/Movies";
+            String user = "root";
+            String pw = "PassWord";
+            con = DriverManager.getConnection(url, user, pw);
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+            System.exit(0);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            System.exit(0);
+        }
+        return con;
+    }
+
+    private static Movie getMovie(ResultSet movies)                             //getMovie method extracts title, year and price from current row - creates movie object
+    {
+        try {
+            String title = movies.getString("Title");
+            int year = movies.getInt("Year");
+            double price = movies.getDouble("Price");
+            return new Movie(title, year, price);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    private static class Movie                                                  //Movie class = inner class
+    {
+
+        public String title;
+        public int year;
+        public double price;
+
+        public Movie(String title, int year, double price) {
+            this.title = title;
+            this.year = year;
+            this.price = price;
+        }
+    }
+
 }
