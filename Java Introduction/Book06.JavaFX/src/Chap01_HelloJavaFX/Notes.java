8f3ff6dd57f8473d49e22871e91d7df82d403bea
Neo Lichaba <neolicham@gmail.com>
2022-10-11 23:59:34 +0200

    ClickMe program - WIP

--- Java Introduction/Book06.JavaFX/src/Chap01_HelloJavaFX/Notes.java
+++ Java Introduction/Book06.JavaFX/src/Chap01_HelloJavaFX/Notes.java
@@ -15,6 +15,33 @@ package Chap01_HelloJavaFX;
         » 3-D objects: You can draw three-dimensional objects such as cubes, cylinders, 
         spheres, and more complex shapes.
         » Touch interface: JavaFX can handle touchscreen devices, such as smartphones and tab
+
+ * Extending Application Class (refer to ClickMe examples)
+   ---------------------------------------------------------
+    - The Application class is responsible for managing what is called the life cycle of a 
+      JavaFX application. The life cycle consists of the following steps:
+        1. Create an instance of the Application class.
+        2. Call the init method.
+        The default implementation of the init method does nothing, but you can 
+        override the init method to provide any processing you want to be performed before the application’s user interface displays.
+        3. Call the start method.
+        The start method is an abstract method, which means that there is no default 
+        implementation provided as a part of the Application class. Therefore, you 
+        must provide your own version of the start method. The start method is 
+        responsible for building and displaying the user interface. 
+        4. Wait for the application to end, which typically happens when the user 
+        signals the end of the program by closing the main application window or 
+        choosing the program’s exit command.
+        During this time, the application isn’t really idle. Instead, it’s busy performing 
+        actions in response to user events, such as clicking a button or choosing an 
+        item from a drop-down list.
+        5. Call the stop method.
+        Like the init method, the default implementation of the stop method doesn’t 
+        do anything, but you can override it to perform any processing necessary as 
+        the program terminates, such as closing database resources or saving files.
+
+   * Launching The Application (refer to ClickMe examples)
+   ---------------------------------------------------------
  *
  * @author Neo
  */
--- Java Introduction/Book06.JavaFX/src/Chap01_HelloJavaFX/P566_ClickMe.java
+++ Java Introduction/Book06.JavaFX/src/Chap01_HelloJavaFX/P566_ClickMe.java
@@ -1,46 +1,56 @@
 
 package Chap01_HelloJavaFX;
 
-import javafx.application.Application;
+import javafx.application.Application;                                          //core class on which all JavaFX applications depend
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+import javafx.scene.Scene;                                                      //container that holds all UI elements displayed by program
+import javafx.scene.control.Button;                                             //individual user interface controls defined
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;                                           //layout manager - determines position of each control displayed on UI
+import javafx.stage.Stage;                                                      //top level container - application's UI elements are displayed
 
 /* Listing 1.1
-
+ * 
  *
  * @author User
  */
-public class ClickMe extends Application {
+public class P566_ClickMe extends Application {
 
     public static void main(String[] args) {
-        launch(args);
+        launch(args);                                                           //Application class' launch method called - starts JavaFX app
+                                                                                //instance of Application class is created; JavaFX lifecycle is started
     }
-    Button btn;
+    Button btn;                                                                 //btn with Button type created
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) {                                     //start method accepts parameter on which app's UI will be displayed
+                                                                                //when start method called, app passes through main stage (aka primary stage parameter)
         // Create the button
-        btn = new Button();
-        btn.setText("Click me please!");
-        btn.setOnAction(e -> buttonClick());
+        btn = new Button();                                                     //btn ctrl created called btn
+        btn.setText("Click me please!");                                        //btn text set
+        btn.setOnAction(e -> buttonClick());                                    //method setOnAction called of btn class when btnClicked
+                                                                                //e - object of ActionEvent
         // Add the button to a layout pane
-        BorderPane pane = new BorderPane();
+        BorderPane pane = new BorderPane();                                     //instance of layout pane created - border pane
+                                                                                //contents arranged into 5 general regions 
+                                                                                //Border pane ideal for layouts which you have elements - menu and toolbar, status,optional panes etc.
         pane.setCenter(btn);
         // Add the layout pane to a scene
-        Scene scene = new Scene(pane, 300, 250);
-        // Finalize and show the stage
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("The Click Me App");
-        primaryStage.show();
+        Scene scene = new Scene(pane, 300, 250);                                //scene displays layout pane
+                                                                                //scene class constructor called - accepts 3 arguments
+                                                                                //pane - root node to be displayed by the scene; width and height of scene in pixels
+        // Finalize and show the stage                                          represents nodes (ctls and layout panes) displayed in application
+                                                                                //stage = window in which the scene is displayed     
+        primaryStage.setScene(scene);                                           //setScene method of primary stage called to set the scene to be displayed
+        primaryStage.setTitle("The Click Me App");                              //setTitle method - title set
+        primaryStage.show();                                                    //show method called, displaying app window to user
     }
 
-    public void buttonClick() {
-        if (btn.getText() == "Click me please!") {
-            btn.setText("You clicked me!");
+    public void buttonClick() {                                                 
+        if (btn.getText() == "Click me please!") {                              //if statement uses to change text displayed by button
+                                                                                //getText - returns text displayed by btn in string format
+            btn.setText("You clicked me!");                                     //setText sets text displayed by the button
         } else {
             btn.setText("Click me please!");
         }
