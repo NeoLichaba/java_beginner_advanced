51a04cb742f6b1b2b9c82e5a24e99b954259f037
Neo Lichaba <98019180+NeoLichaba@users.noreply.github.com>
2022-10-16 18:52:48 +0200

    Geeks For Geeks Quiz Results - Java Constructors uploaded

--- Java Introduction/GeeksForGeeksQuiza/src/geeksforgeeksquiza/Main.java
+++ Java Introduction/GeeksForGeeksQuiza/src/geeksforgeeksquiza/Main.java
@@ -5,18 +5,35 @@ package geeksforgeeksquiza;
  *
  * @author User
  */
-
 class Test
 {
+    static int a;
+     
+    static
+    {
+        a = 4;
+        System.out.println ("inside static blockn");
+        System.out.println ("a = " + a);
+    }
+     
+    Test()
+    {
+        System.out.println ("ninside constructorn");
+        a = 10;
+    }
+     
+    public static void func()
+    {
+        a = a + 1;
+        System.out.println ("a = " + a);
+    }
+     
     public static void main(String[] args)
     {
-        Double object = new Double("2.4");
-        int a = object.intValue();
-        byte b = object.byteValue();
-        float d = object.floatValue();
-        double c = object.doubleValue();
  
-        System.out.println(a + b + c + d );
+        Test obj = new Test();
+        obj.func();
  
     }
 }
+  
