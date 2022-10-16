1eb413a436d677762a7e319721489d407f6433f5
Neo Lichaba <98019180+NeoLichaba@users.noreply.github.com>
2022-10-16 19:43:21 +0200

    Geeks For Geeks Quiz Results - Java Packages

--- Java Introduction/GeeksForGeeksQuiza/src/geeksforgeeksquiza/Main.java
+++ Java Introduction/GeeksForGeeksQuiza/src/geeksforgeeksquiza/Main.java
@@ -5,55 +5,12 @@ package geeksforgeeksquiza;
  *
  * @author User
  */
-class Test
-{   int count = 0;
- 
-    void A() throws Exception
-    {
-        try
-        {
-            count++;
-             
-            try
-            {
-                count++;
- 
-                try
-                {
-                    count++;
-                    throw new Exception();
- 
-                }
-                 
-                catch(Exception ex)
-                {
-                    count++;
-                    throw new Exception();
-                }
-            }
-             
-            catch(Exception ex)
-            {
-                count++;
-            }
-        }
-         
-        catch(Exception ex)
-        {
-            count++;
-        }
- 
-    }
- 
-    void display()
-    {
-        System.out.println(count);
-    }
- 
-    public static void main(String[] args) throws Exception
-    {
-        Test obj = new Test();
-        obj.A();
-        obj.display();
-    }
+import static java.lang.System.*;
+   
+class StaticImportDemo
+{
+   public static void main(String args[])
+   {      
+        out.println("GeeksforGeeks");
+   }
 }
